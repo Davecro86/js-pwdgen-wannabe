337e6eeb75db49c2ecf05369b252e2080f2c9ff8
Davide Crocitta <davidecrocitta@libero.it>
2020-07-07 03:56:55 +0200

    Esercizio
    Password generator

---
+++ js/script.js
@@ -0,0 +1,6 @@
+var name = prompt('Inserisci il tuo nome');
+var surname = prompt('Inserisci il tuo cognome');
+var color = prompt('Inserisci il tuo colore preferito');
+var number = 19;
+document.getElementById('C4v0l3tt1')
+.innerHTML = name + surname + color + number;
